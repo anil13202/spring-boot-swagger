95af0d35ccd7361ff4c616f0bdc955b550c22ed7
anil13202 <89154564+anil13202@users.noreply.github.com>
2021-08-18 13:41:23 -0400

    Changed the order of jason or output display

--- src/main/java/com/example/springbootswagger2/model/Student.java
+++ src/main/java/com/example/springbootswagger2/model/Student.java
@@ -3,12 +3,13 @@ package com.example.springbootswagger2.model;
 import io.swagger.annotations.ApiModelProperty;
 
 public class Student {
+	@ApiModelProperty(notes = "Class of the Student",name="cls",required=true,value="test class")
+	private String cls;
 	@ApiModelProperty(notes = "Name of the Student",name="name",required=true,value="test name")
 	private String name;
 	@ApiModelProperty(notes = "Country of the Student",name="country",required=true,value="test country")
 	private String country;
-	@ApiModelProperty(notes = "Class of the Student",name="cls",required=true,value="test class")
-	private String cls;
+
 
 	public Student(String name, String cls, String country) {
 		super();
