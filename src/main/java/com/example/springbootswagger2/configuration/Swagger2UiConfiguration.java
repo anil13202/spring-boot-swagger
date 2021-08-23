fd740e2b1e36d292e1e35220e65d0d546239ff42
anil13202 <89154564+anil13202@users.noreply.github.com>
2021-08-22 20:38:19 -0400

    Update Swagger2UiConfiguration.java

--- src/main/java/com/example/springbootswagger2/configuration/Swagger2UiConfiguration.java
+++ src/main/java/com/example/springbootswagger2/configuration/Swagger2UiConfiguration.java
@@ -11,7 +11,7 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
+//comment
 @Configuration
 @EnableSwagger2
 public class Swagger2UiConfiguration extends WebMvcConfigurerAdapter  {
