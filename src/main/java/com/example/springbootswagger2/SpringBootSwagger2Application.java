f5a7ac9937c119382d71d704e9aeef4eeeef09ef
anil13202 <89154564+anil13202@users.noreply.github.com>
2021-08-29 21:51:07 -0400

    Commit

--- src/main/java/com/example/springbootswagger2/SpringBootSwagger2Application.java
+++ src/main/java/com/example/springbootswagger2/SpringBootSwagger2Application.java
@@ -2,8 +2,11 @@ package com.example.springbootswagger2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 //just a comment HOLA
 @SpringBootApplication
+@EnableSwagger2
 public class SpringBootSwagger2Application {
 
 	public static void main(String[] args) {
